65a35296f2c2bb4776309fd08067fd6b8128985a
Jimmy Tanagra <jcode@tanagra.id.au>
2025-03-27 15:45:21 +1000

    Create GEM_HOME directory

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import javax.script.ScriptEngine;
 
 public class App {
@@ -8,9 +11,18 @@ public class App {
         ScriptEngine engine = factory.getScriptEngine();
         engine.eval("puts 'Hello, JRuby!'");
 
+        Path path = Paths.get(App.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+
+        engine.put("GEM_HOME", path.resolveSibling("gems").toString());
         engine.eval("""
+                ENV["GEM_HOME"] = GEM_HOME
+
                 require "bundler/inline"
                 require "openssl"
+
+                puts "JRuby Version: #{JRUBY_VERSION}"
+                puts "GEM_HOME: #{GEM_HOME}\n\n"
+                Dir.mkdir(GEM_HOME) unless Dir.exist?(GEM_HOME)
 
                 gemfile(true) do
                     source "https://rubygems.org"
