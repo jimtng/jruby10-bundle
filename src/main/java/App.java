83fa751c2b73b306a4fcec6ebd6b1fa3ead7584b
Jimmy Tanagra <jcode@tanagra.id.au>
2025-04-01 10:05:41 +1000

    (NameError) undefined local variable or method 'it' for main
    Signed-off-by: Jimmy Tanagra <jcode@tanagra.id.au>

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,32 +1,17 @@
 package com.example;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import javax.script.ScriptEngine;
 
 public class App {
     public static void main(String[] args) throws Exception {
         javax.script.ScriptEngineFactory factory = new org.jruby.embed.jsr223.JRubyEngineFactory();
         ScriptEngine engine = factory.getScriptEngine();
-        engine.eval("puts 'Hello, JRuby!'");
-
-        Path path = Paths.get(App.class.getProtectionDomain().getCodeSource().getLocation().toURI());
-
-        engine.put("GEM_HOME", path.resolveSibling("gems").toString());
         engine.eval("""
-                ENV["GEM_HOME"] = GEM_HOME
-
-                require "bundler/inline"
-                require "openssl"
-
                 puts "JRuby Version: #{JRUBY_VERSION}"
-                puts "GEM_HOME: #{GEM_HOME}\n\n"
-                Dir.mkdir(GEM_HOME) unless Dir.exist?(GEM_HOME)
+                puts "JRuby Revision: #{JRUBY_REVISION}"
 
-                gemfile(true) do
-                    source "https://rubygems.org"
-                    gem "httparty"
+                [1, 2].each do 
+                    puts it
                 end
                 """);
     }
